2341cda090c262edbe7cf4a445bd456c7cc401af
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-10 17:46:37 +0100

    more scaffolding

--- index.ios.js
+++ index.ios.js
@@ -2,17 +2,32 @@ var React = require('react-native');
 var {
   Text,
   View,
-  AppRegistry
+  AppRegistry,
+  StyleSheet
 } = React;
 
 var Stopwatch = React.createClass({
   render: function() {
     return <View>
-      <Text>
-        00:00.00
-      </Text>
-      {this.startStopButton()}
-      {this.lapButton()}
+
+      <View>{/* Yellow */}
+        <View>{/* Red */}
+          <Text>
+            00:00.00
+          </Text>
+          <View>{/* Green */}
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+      </View>
+
+      <View>{/* Blue */}
+        <Text>
+          I am a list of laps
+        </Text>
+      </View>
+
     </View>
   },
   startStopButton: function() {
@@ -25,6 +40,10 @@ var Stopwatch = React.createClass({
       <Text>Lap</Text>
     </View>
   }
+});
+
+var styles = StyleSheet.create({
+
 });
 
 AppRegistry.registerComponent('stopwatch', () => Stopwatch);
