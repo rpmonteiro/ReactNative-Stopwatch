9a6d2ba4ea84682b5d447b364356e3f6de94289f
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-10 17:28:23 +0100

    keeping the render method clean by extracting views to their own helper methods

--- index.ios.js
+++ index.ios.js
@@ -7,7 +7,23 @@ var {
 
 var Stopwatch = React.createClass({
   render: function() {
-
+    return <View>
+      <Text>
+        00:00.00
+      </Text>
+      {this.startStopButton()}
+      {this.lapButton()}
+    </View>
+  },
+  startStopButton: function() {
+    return <View>
+      <Text>Start</Text>
+    </View>
+  },
+  lapButton: function() {
+    return <View>
+      <Text>Lap</Text>
+    </View>
   }
 });
 
